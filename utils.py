9ae706d1e195e9fb75e5adaeb5d09f70ecda0f9c
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-06-16 10:12:16 +0530

    data loaded

--- utils.py
+++ utils.py
@@ -11,3 +11,17 @@ def initialize_bias(shape,dtype = "", name = ""):
     
     return np.random.normal(loc= 0.0, scale= 0.01, size=shape)
 
+
+def get_batch(batch, size, s = "train"):
+
+    if s == 'train':
+        X = Xtrain
+        categories = train_classes
+
+    else:
+        X  = Xval
+        categories = val_classes 
+
+    n_classes, n_examples, w, h = X.shape 
+
+    
