57f539c0cf474f5c844b9be645c8837adf117baa
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-06-16 15:55:34 +0530

    progress

--- train.py
+++ train.py
@@ -1,6 +1,10 @@
 import tensorflow as tf 
 import numpy as np 
 import pickle
+import os
+import time
+from sklearn.utils import shuffle
+
 
 from model import siamese_network
 
@@ -29,7 +33,6 @@ print(list(val_classes.keys()))
 
 
 def get_batch(batch_size,s="train"):
-    """Create batch of n pairs, half same class, half different class"""
     if s == 'train':
         X = Xtrain
         categories = train_classes
@@ -64,3 +67,85 @@ def get_batch(batch_size,s="train"):
     
     return pairs, targets
 
+
+def generate(batch_size, s="train"):
+    while True:
+        pairs, targets = get_batch(batch_size,s)
+        yield (pairs, targets)
+
+def make_oneshot_task(N, s="val", language=None):
+    if s == 'train':
+        X = Xtrain
+        categories = train_classes
+    else:
+        X = Xval
+        categories = val_classes
+    n_classes, n_examples, w, h = X.shape
+    
+    indices = np.random.randint(0, n_examples,size=(N,))
+    if language is not None: # if language is specified, select characters for that language
+        low, high = categories[language]
+        if N > high - low:
+            raise ValueError("This language ({}) has less than {} letters".format(language, N))
+        categories = np.random.choice(range(low,high),size=(N,),replace=False)
+
+    else: 
+        categories = np.random.choice(range(n_classes),size=(N,),replace=False)            
+    true_category = categories[0]
+    ex1, ex2 = np.random.choice(n_examples,replace=False,size=(2,))
+    test_image = np.asarray([X[true_category,ex1,:,:]]*N).reshape(N, w, h,1)
+    support_set = X[categories,indices,:,:]
+    support_set[0,:,:] = X[true_category,ex2]
+    support_set = support_set.reshape(N, w, h,1)
+    targets = np.zeros((N,))
+    targets[0] = 1
+    targets, test_image, support_set = shuffle(targets, test_image, support_set)
+    pairs = [test_image,support_set]
+
+    return pairs, targets
+
+
+def test_oneshot(model, N, k, s = "val", verbose = 0):
+    """Test average N way oneshot learning accuracy of a siamese neural net over k one-shot tasks"""
+    n_correct = 0
+    if verbose:
+        print("Evaluating model on {} random {} way one-shot learning tasks ... \n".format(k,N))
+    for i in range(k):
+        inputs, targets = make_oneshot_task(N,s)
+        probs = model.predict(inputs)
+        if np.argmax(probs) == np.argmax(targets):
+            n_correct+=1
+    percent_correct = (100.0 * n_correct / k)
+    if verbose:
+        print("Got an average of {}% {} way one-shot learning accuracy \n".format(percent_correct,N))
+    return percent_correct
+
+
+
+evaluate_every = 200 
+batch_size = 32
+n_iter = 20000 
+N_way = 20 
+n_val = 250 
+best = -1
+
+
+
+model_path = './weights/'
+
+
+print("Starting training process!")
+print("-------------------------------------")
+t_start = time.time()
+for i in range(1, n_iter+1):
+    (inputs,targets) = get_batch(batch_size)
+    loss = model.train_on_batch(inputs, targets)
+    if i % evaluate_every == 0:
+        print("\n ------------- \n")
+        print("Time for {0} iterations: {1} mins".format(i, (time.time()-t_start)/60.0))
+        print("Train Loss: {0}".format(loss)) 
+        val_acc = test_oneshot(model, N_way, n_val, verbose=True)
+        model.save_weights(os.path.join(model_path, 'weights.{}.h5'.format(i)))
+        if val_acc >= best:
+            print("Current best: {0}, previous best: {1}".format(val_acc, best))
+            best = val_acc
