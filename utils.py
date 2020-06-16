c51f0fdf1eb7b750c7b8a22d6d3530ddcd1df4e1
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-06-16 09:01:19 +0530

    minor bugfix

--- utils.py
+++ utils.py
@@ -2,12 +2,12 @@ import numpy as np
 import tensorflow as tf 
 
 
-def initialize_weights(shape):
+def initialize_weights(shape, dtype = "", name = ""):
      
      return np.random.normal(loc= 0.0, scale= 0.01, size = shape)
 
 
-def initialize_bias(shape):
+def initialize_bias(shape,dtype = "", name = ""):
     
     return np.random.normal(loc= 0.0, scale= 0.01, size=shape)
 
