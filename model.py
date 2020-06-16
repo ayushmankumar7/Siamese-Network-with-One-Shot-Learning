3922c93dffc9ba110305c1ba34f53930c849b5ff
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-06-16 09:01:37 +0530

    model final

--- model.py
+++ model.py
@@ -15,21 +15,21 @@ def siamese_network(input_shape):
 
     model = Sequential([
 
-        Conv2D(64, (10,10), activation = 'relu', input_shape = input_shape , kernel_initializer = initialize_weights, kernel_regulatizer = tf.keras.regularizers.l2(2e-4)),
+        Conv2D(64, (10,10), activation = 'relu', input_shape = input_shape , kernel_initializer = initialize_weights, kernel_regularizer = tf.keras.regularizers.l2(2e-4)),
         MaxPooling2D(),
-        Conv2D(128, (7,7), activation = 'relu', kernel_initializer = initialize_weights, bias_initializer = initialize_bias, kernel_regulatizer = tf.keras.regularizers.l2(2e-4)),
+        Conv2D(128, (7,7), activation = 'relu', kernel_initializer = initialize_weights, bias_initializer = initialize_bias, kernel_regularizer = tf.keras.regularizers.l2(2e-4)),
         MaxPooling2D(),
-        Conv2D(128, (4,4), activation = 'relu', kernel_initializer = initialize_weights, bias_initializer = initialize_bias, kernel_regulatizer = tf.keras.regularizers.l2(2e-4)),
+        Conv2D(128, (4,4), activation = 'relu', kernel_initializer = initialize_weights, bias_initializer = initialize_bias, kernel_regularizer = tf.keras.regularizers.l2(2e-4)),
         MaxPooling2D(),
-        Conv2D(256, (4,4), activation = 'relu', kernel_initializer = initialize_weights, bias_initializer = initialize_bias, kernel_regulatizer = tf.keras.regularizers.l2(2e-4)),
+        Conv2D(256, (4,4), activation = 'relu', kernel_initializer = initialize_weights, bias_initializer = initialize_bias, kernel_regularizer = tf.keras.regularizers.l2(2e-4)),
         Flatten(),
-        Dense(4096, activation = 'sigmoid', kernel_initializer = initialize_weights, bias_initializer = initialize_bias, kernel_regulatizer = tf.keras.regularizers.l2(1e-3))
+        Dense(4096, activation = 'sigmoid', kernel_initializer = initialize_weights, bias_initializer = initialize_bias, kernel_regularizer = tf.keras.regularizers.l2(1e-3))
     ])
 
     encoded_1 = model(input1)
     encoded_2 = model(input2)
 
-    L1_layer = Lambda(lambda x: abs(x[0] - x[1]))
+    L1_layer = Lambda(lambda x: tf.keras.backend.abs(x[0] - x[1]))
     L1_distance = L1_layer([encoded_1, encoded_2])
 
     prediction = Dense(1, activation = 'sigmoid')(L1_distance)
